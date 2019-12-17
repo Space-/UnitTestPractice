acf3e93523b40b7836d775011b5936922eaf68ec
Bruce <gkm85663@gmail.com>
2019-12-18 02:08:10 +0800

    upload file

--- UnitTestPractice/UnitTest1.cs
+++ UnitTestPractice/UnitTest1.cs
@@ -14,86 +14,87 @@ namespace Tests
         [Test]
         public void Test_Charge_Customer_Count()
         {
-            //arrange
-            ICheckInFee stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
-            Pub target = new Pub(stubCheckInFee);
-
+            // arrange
+            var stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
+            var target = new Pub(stubCheckInFee);
             stubCheckInFee.Stub(x => x.GetFee(Arg<Customer>.Is.Anything)).Return(100);
 
-            var customers = new List<Customer>
+            var customers = new List<Customer>()
             {
-                new Customer {IsMale = true},
-                new Customer {IsMale = false},
-                new Customer {IsMale = false},
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = false},
+                new Customer(){IsMale = false}
             };
+
+            customers.ForEach(x => x.IsMale = false);
 
             decimal expected = 1;
 
-            //act
+            // act
             var actual = target.CheckIn(customers);
 
-            //assert
-            Assert.AreEqual(expected, actual);
+            // assert
+            Assert.Equals(expected, actual);
+        }
+    }
+
+    public interface ICheckInFee
+    {
+        decimal GetFee(Customer customer);
+    }
+
+    public class Customer
+    {
+        public bool IsMale { get; set; }
+
+        public int Seq { get; set; }
+    }
+
+    public class Pub
+    {
+        private ICheckInFee _checkInFee;
+        private decimal _inCome = 0;
+
+        public Pub(ICheckInFee checkInFee)
+        {
+            this._checkInFee = checkInFee;
         }
 
-        public interface ICheckInFee
+        /// <summary>
+        /// 入場
+        /// </summary>
+        /// <param name="customers"></param>
+        /// <returns>收費的人數</returns>
+        public int CheckIn(List<Customer> customers)
         {
-            decimal GetFee(Customer customer);
-        }
+            var result = 0;
 
-        public class Customer
-        {
-            public bool IsMale { get; set; }
-
-            public int Seq { get; set; }
-        }
-
-        public class Pub
-        {
-            private ICheckInFee _checkInFee;
-            private decimal _inCome = 0;
-
-            public Pub(ICheckInFee checkInFee)
+            foreach (var customer in customers)
             {
-                this._checkInFee = checkInFee;
-            }
+                var isFemale = !customer.IsMale;
 
-            /// <summary>
-            /// 入場
-            /// </summary>
-            /// <param name="customers"></param>
-            /// <returns>收費的人數</returns>
-            public int CheckIn(List<Customer> customers)
-            {
-                var result = 0;
-
-                foreach (var customer in customers)
+                //女生免費入場
+                if (isFemale)
                 {
-                    var isFemale = !customer.IsMale;
-
-                    //女生免費入場
-                    if (isFemale)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        //for stub, validate status: income value
-                        //for mock, validate only male
-                        this._inCome += this._checkInFee.GetFee(customer);
-
-                        result++;
-                    }
+                    continue;
                 }
+                else
+                {
+                    //for stub, validate status: income value
+                    //for mock, validate only male
+                    this._inCome += this._checkInFee.GetFee(customer);
 
-                //for stub, validate return value
-                return result;
+                    result++;
+                }
             }
 
-            public decimal GetInCome()
-            {
-                return this._inCome;
-            }
+            //for stub, validate return value
+            return result;
+        }
+
+        public decimal GetInCome()
+        {
+            return this._inCome;
         }
     }
 }
